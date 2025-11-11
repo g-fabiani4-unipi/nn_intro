8b8628fc4f820ae8810e2974f18a58cd0ebb2ab4
Giulia Fabiani <g.fabiani4@studenti.unipi.it>
2025-11-11 10:58:26 +0100

    Add transitions to mnist section

--- src/GraphComponents/Pixel.svelte
+++ src/GraphComponents/Pixel.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { tweened } from 'svelte/motion';
+	import { fly } from 'svelte/transition';
 
 	export let x;
 	export let y;
@@ -15,8 +16,14 @@
 	const corner = tweened(0, { duration: 500 });
 	const tWidth = tweened(width, { duration: 500 });
 
-	$: tX.set(x, { duration: delay ? 500 : 1000, delay: delay ? index * 50 : 0 });
-	$: tY.set(y, { duration: delay ? 500 : 1000, delay: delay ? index * 50 : 0 });
+	$: tX.set(x, {
+		duration: delay ? 500 : 1000,
+		delay: delay ? index * 50 : 500,
+	});
+	$: tY.set(y, {
+		duration: delay ? 500 : 1000,
+		delay: delay ? index * 50 : 500,
+	});
 	$: round ? corner.set(50) : corner.set(0);
 	$: tWidth.set(width);
 </script>
@@ -30,6 +37,8 @@
 	stroke-width={strokeWidth}
 	rx={$corner}
 	ry={$corner}
+	in:fly={{ duration: 1000, x: 200, delay: 50 * index }}
+	out:fly={{ duration: 1000, x: 200 }}
 />
 
 <style>
