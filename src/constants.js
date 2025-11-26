092c5d9f25156e8749a1844ae5ca91a1a237aded
Giulia Fabiani <g.fabiani4@studenti.unipi.it>
2025-11-26 12:05:46 +0100

    Add text where possible

--- src/constants.js
+++ src/constants.js
@@ -12,7 +12,66 @@ export const allSteps = {
     },
     {
       name: 'enter_network',
-      text: `<p>And now we have our perceptron. It has <strong>two input units</strong> in order to model the fact that the AND function takes two arguments.</p>`
+      text: `<p>And now we have our perceptron. It has <strong>two input units</strong> in order to model the fact that the AND function takes two arguments.</p>
+      <p>The perceptron equation for a perceptron with two inputs is the equation of a line</p>
+      <math
+			xmlns="http://www.w3.org/1998/Math/MathML"
+			display="block"
+      class="math-block"
+		>
+			<msub><mi>w</mi><mn>1</mn></msub>
+			<msub><mi>x</mi><mn>1</mn></msub>
+			<mo>+</mo>
+			<msub><mi>w</mi><mn>2</mn></msub>
+			<msub><mi>x</mi><mn>2</mn></msub>
+			<mo>+</mo>
+			<msub><mi>w</mi><mn>0</mn></msub>
+			<mo>=</mo>
+			<mn>0</mn>
+		</math>
+    <p>which in explicit form would be</p>
+	<math
+		xmlns="http://www.w3.org/1998/Math/MathML"
+		display="block"
+		class="math-block"
+	>
+		<msub><mi>x</mi><mn>2</mn></msub><mo>=</mo>
+		<mo>-</mo>
+		<mfrac>
+			<mrow> <msub><mi>w</mi><mn>1</mn></msub> </mrow>
+			<mrow> <msub><mi>w</mi><mn>2</mn></msub> </mrow>
+		</mfrac>
+		<msub><mi>x</mi><mn>1</mn></msub>
+		<mo>-</mo>
+		<mfrac>
+			<mrow> <msub><mi>w</mi><mn>0</mn></msub> </mrow>
+			<mrow> <msub><mi>w</mi><mn>2</mn></msub> </mrow>
+		</mfrac>
+	</math>
+  <p>where 	<math
+		xmlns="http://www.w3.org/1998/Math/MathML"
+		display="inline"
+	>
+		<mo>-</mo>
+		<mfrac>
+			<mrow> <msub><mi>w</mi><mn>1</mn></msub> </mrow>
+			<mrow> <msub><mi>w</mi><mn>2</mn></msub> </mrow>
+		</mfrac>
+	</math> is the slope and
+  <math
+		xmlns="http://www.w3.org/1998/Math/MathML"
+		display="inline"
+	>
+		<mo>-</mo>
+		<mfrac>
+			<mrow> <msub><mi>w</mi><mn>0</mn></msub> </mrow>
+			<mrow> <msub><mi>w</mi><mn>2</mn></msub> </mrow>
+		</mfrac>
+	</math>
+  is the intercept.
+  </p>
+    `
+
     },
     {
       name: 'enter_data',
@@ -25,7 +84,8 @@ export const allSteps = {
     },
     {
       name: 'perceptron_rule_start',
-      text: '<p>Show the perceptron learning rule.</p><p>I am resetting the parameters, but notice that this is somewhat hacky and prevents changing the params in the current step. You should also disable the inputs</p>'
+      text: '<p>The perceptron is able to set its own parameters by correcting the classification error on a training set.</p>\
+      <p><span>Example 1</span> is classified correctly, so no change is necessary.</p>'
     },
     {
       name: 'perceptron_rule_2',
@@ -33,35 +93,35 @@ export const allSteps = {
     },
     {
       name: 'perceptron_rule_3',
-      text: '<p>Hardcoded the parameters update</p>'
+      text: '<p>Update the parameters.</p>'
     },
     {
       name: 'perceptron_rule_4',
-      text: '<p>This example is okay</p>'
+      text: '<p>This example is okay.</p>'
     },
     {
       name: 'perceptron_rule_5',
-      text: '<p>This is not</p>'
+      text: '<p>This, however, is not</p>'
     },
     {
       name: 'perceptron_rule_6',
-      text: '<p>Applying change</p>'
+      text: '<p>Applying change.</p>'
     },
     {
       name: 'perceptron_rule_7',
-      text: '<p>Applying change</p>'
+      text: '<p>Applying change.</p>'
     },
     {
       name: 'perceptron_rule_8',
-      text: '<p>Sh#t, now 2 is misclassified</p>'
+      text: '<p>Now <span class="example">example 2</span> is misclassified.</p>'
     },
     {
       name: 'perceptron_rule_9',
-      text: '<p>This is the correct step here (and now 4 is misclassified)</p>'
+      text: '<p>Applying change. Now <span class="example">example 4</span> is misclassified.</p>'
     },
     {
       name: 'perceptron_rule_end',
-      text: '<p>Solved.</p>'
+      text: '<p>Applying change. All examples are classified correctly, thus the algorithm terminates.</p>'
     },
     {
       name: 'xor_start',
@@ -80,15 +140,17 @@ export const allSteps = {
     },
     {
       name: 'ml_perceptron_start',
-      text: '<p>Enter the multilayer perceptron</p>'
+      text: '<p>Enter the <strong>multilayer perceptron</strong>.</p>\
+      <p>In addition to <strong>input and output units</strong>, the multilayer perceptron has one or more layers of <strong>hidden units</strong> (here h<sub>1</sub> and h<sub>2</sub>).</p>'
     },
     {
       name: 'ml_perceptron_2',
-      text: "<p>Write something here</p>"
+      text: "<p>Because of the presence of hidden layers, the multilayer perceptron can solve problems that are not linearly separable.</p>\
+      <p>Can you find a configuration of parameters that solves the XOR problem?</p>"
     },
     {
       name: 'ml_perceptron_3',
-      text: "<p>A configuration of parameters that solves the XOR problem. You can inspect the decision boundary of each hidden node by clicking on them.</p>"
+      text: "<p>Here we show a configuration of parameters that solves the XOR problem. You can inspect the decision boundary of each hidden node by clicking on them.</p>"
     },
     {
       name: 'ml_perceptron_end',
@@ -99,15 +161,16 @@ export const allSteps = {
   mnist: [
     {
       name: 'start',
-      text: "<p>This is the first step in the mnist section</p>"
+      text: "<p>Multilayer perceptrons can be trained to solve also more interesting problems, as for example to <strong>classify handwritten digits</strong></p>\
+      <p>In this example we represent each digit as a <strong>8x8 grid of pixels</strong>.</p>"
     },
     {
       name: 'decompose_input',
-      text: "<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.Aliquid doloremque, enim dolores nam aliquam tenetur dignissimos ullam itaque odit ipsum consequuntur officiis, dolorum minima vel ipsa facilis vero architecto.Deleniti.</p>"
+      text: "<p>We represent the grid as a vector of 64 numbers. Each number represent how dark the corresponding pixel is.</p>"
     },
     {
       name: 'enter_input_nodes',
-      text: '<p>So we can build our perceptron: we have 64 Input nodes</p>'
+      text: '<p>And now we can build our perceptron: we have 64 input units.</p>'
     },
     {
       name: 'enter_output_nodes',
@@ -119,42 +182,44 @@ export const allSteps = {
     },
     {
       name: 'enter_links',
-      text: '<p>Una botta di calcoli</p>'
+      text: '<p>Can you compute how many <strong>parameters</strong> should the perceptron have?</p>'
     },
 
     {
       name: 'end',
-      text: '<p>Click on the test <span class="example">examples</span> to see the model output</p>'
+      text: '<p>The perceptron has already been trained. Click on the test <span class="example">examples</span> to see the model output</p>'
     }
   ],
   skipgram: [
     {
       name: 'start',
-      text: '<p>Here starts the skipgram section</p>'
+      text: '<p>Here we show only the forward pass for SkipGram</p>'
     },
     {
       name: 'activate_input',
-      text: '<p>Activate node</p>'
+      text: '<p>Suppose we encounter the word <em>dog</em>.</p>'
     },
     {
       name: 'activate_output_1',
-      text: '<p>Activate output 1</p>'
+      text: '<p>Compute the activation for output <em>dog</em></p>'
     },
     {
       name: 'activate_output_2',
-      text: '<p>Activate output 2</p>'
+      text: '<p>Compute the activation for output <em>cat</em></p>'
+
     },
     {
       name: 'activate_output_3',
-      text: '<p>Activate output 3</p>'
+      text: '<p>Compute the activation for output <em>eat</em></p>'
+
     },
     {
       name: 'activate_output_4',
-      text: '<p>Activate output 4</p>'
+      text: '<p>Compute the activation for output <em>bark</em></p>'
     },
     {
       name: 'softmax',
-      text: '<p>Pass network outputs in softmax</p>'
+      text: '<p>The activations of the output units are passed into a <em>softmax</em> in order to output probabilities.</p>'
     }
   ]
 };
