7809e574dba971eb8c9ec8c11c3fe6ccc03a6629
Giulia Fabiani <g.fabiani4@studenti.unipi.it>
2026-07-04 10:35:02 +0200

    [license] Add Share Alike clause to content license

--- src/Sections/Backmatter.svelte
+++ src/Sections/Backmatter.svelte
@@ -31,18 +31,21 @@
 	</div>
 	<div class="footer">
 		<p>
-			<a href="https://g-fabiani4-unipi.github.io/nn_intro/"
-				>Introduction to Neural Networks</a
-			>
+			<a href="https://g-fabiani4-unipi.github.io/nn_intro/">The Perceptron</a>
 			© 2025 by
 			<a href="https://github.com/g-fabiani4-unipi">Giulia Fabiani</a>
 			is licensed under
-			<a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a><img
+			<a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a
+			><img
 				src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"
 				alt=""
 				style="max-width: 1em;max-height:1em;margin-left: .2em;"
 			/><img
 				src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
+				alt=""
+				style="max-width: 1em;max-height:1em;margin-left: .2em;"
+			/><img
+				src="https://mirrors.creativecommons.org/presskit/icons/sa.svg"
 				alt=""
 				style="max-width: 1em;max-height:1em;margin-left: .2em;"
 			/>
