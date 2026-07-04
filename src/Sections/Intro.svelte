6fc5b02bcf438a6b6f481127268fde7d3187347e
Giulia Fabiani <g.fabiani4@studenti.unipi.it>
2026-07-04 09:57:27 +0200

    [frontmatter] Add subtitle and signifier for scrolling behavior

--- src/Sections/Intro.svelte
+++ src/Sections/Intro.svelte
@@ -4,7 +4,24 @@
 </script>
 
 <div class="hero">
-	<h1>The perceptron</h1>
+	<header>
+		<h1>The perceptron</h1>
+		<p class="lead">A visual introduction to Deep Learning</p>
+	</header>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="48"
+		height="48"
+		fill="currentColor"
+		class="bi bi-mouse-fill"
+		viewBox="0 0 16 16"
+		aria-hidden="true"
+	>
+		<path
+			d="M3 5a5 5 0 0 1 10 0v6a5 5 0 0 1-10 0zm5.5-1.5a.5.5 0 0 0-1 0v2a.5.5 0 0 0 1 0z"
+		/>
+	</svg>
+	<p class="hint">Scroll down to start</p>
 </div>
 <section class="prose">
 	<h2>This is your brain</h2>
@@ -187,8 +204,67 @@
 		margin-top: 5px;
 	}
 
-	h1 {
+	h1,
+	p.lead {
 		backdrop-filter: blur(1px);
 		padding: 10px;
+		margin: 0;
+	}
+
+	h1 {
+		font-size: 3rem;
+	}
+
+	p.lead {
+		/* font-weight: 600; */
+		font-size: 2rem;
+		text-transform: uppercase;
+	}
+
+	.bi.bi-mouse-fill {
+		animation-duration: 3000ms;
+		animation-name: move, fade;
+		animation-iteration-count: infinite;
+	}
+
+	@keyframes move {
+		0% {
+			translate: 0 0;
+		}
+
+		35% {
+			translate: 0 4vh;
+		}
+
+		100% {
+			translate: 0 0;
+		}
+	}
+
+	@keyframes fade {
+		0% {
+			opacity: 1;
+		}
+
+		35% {
+			opacity: 0.2;
+		}
+
+		100% {
+			opacity: 1;
+		}
+	}
+
+	.hint {
+		font-size: smaller;
+		animation: 3000ms fade infinite;
+	}
+
+	.hero > header {
+		height: 80vh;
+		display: flex;
+		place-items: center;
+		flex-direction: column;
+		justify-content: center;
 	}
 </style>
